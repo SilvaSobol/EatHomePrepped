fbf11cfc8f78d13e2e5d6a058ac9090ae273ad29
Pablo Moreno <62684622+PabloMoreno18@users.noreply.github.com>
2020-08-04 21:18:15 -0700

    added login and register

--- eHp/urls.py
+++ eHp/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('register', views.create_user),
+    path('login', views.user_login),
+    path('user/', views.login, name = "login"),
     path('cart/', views.cart, name = "cart"),
     path('menu/', views.item_menu, name ="menu"),
 ]
--- eHp/views.py
+++ eHp/views.py
@@ -15,3 +15,35 @@ def cart(request):
 def item_menu(request):
     context = {}
     return render(request, 'menu.html', context)
+
+def login(request):
+    context = {}
+    return render(request, 'login.html', context)
+    
+def create_user(request):
+    errors = User.objects.basic_validator(request.POST)
+    
+    if errors:
+        for e in errors:
+            error(request, e)
+        return redirect('/')
+    else:
+        user = User.objects.create(first_name=request.POST['fname'], last_name=request.POST['lname'], email=request.POST['email'],password=request.POST['password'])
+        request.session['userID'] = user.id 
+        request.session['name'] = user.first_name
+        return redirect('/dashboard')
+
+def user_login(request):
+    logged_user = User.objects.filter(email=request.POST['email'])
+    if len(logged_user) > 0:
+        logged_user = logged_user[0]
+        if logged_user.password == request.POST['password']:
+            request.session['userID'] = logged_user.id 
+            request.session['name'] = logged_user.first_name 
+            return redirect('/dashboard')
+        elif logged_user.password != request.POST['password']:
+            error(request, "Incorrect password")
+        return redirect ('/')
+    else:
+        error(request, "This email has not been registered.")
+        return redirect('/')
